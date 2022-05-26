25b43b33d2bb87a2f0b15c87a2456f5637570825
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2022-05-26 15:53:14 +0900

    Create No2439.java

---
+++ 반복문/No2439.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		String res="";
+		int j=0;
+		for(int i=0;i<n;i++)
+		{
+		
+		for(j=0;j<(n-i-1);j++)
+		{
+			res=res+" ";
+		}
+		for(j=0;j<=i;j++)
+		{
+			res=res+"*";
+		}
+		res=res+"\n";
+		}
+		System.out.println(res);
+	}
+}
